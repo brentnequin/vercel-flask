c0c6167892bd86c860dbe7b749a44004a53020e1
brentnequin <brentnequin@gmail.com>
2022-12-17 22:38:25 -0600

    more routes

--- api/index.py
+++ api/index.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
+@app.route('/api/about')
 def about():
     return 'About'
